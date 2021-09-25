21954b9968777129b968f09146bc9797ee91c93d
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-25 11:59:17 -0300

    Deleção de notas

--- src/App.js
+++ src/App.js
@@ -5,28 +5,37 @@ import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      notas:[]
+      notas: []
     }
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
     const novoEstado = {
-      notas:novoArrayNotas
+      notas: novoArrayNotas
     }
     this.setState(novoEstado)
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas
+          apagarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas}
+        />
       </section>
     );
   }
